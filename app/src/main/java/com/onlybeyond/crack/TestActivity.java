1dddbc033c129c990f4c8279c88186b2bafd07d8
only <932507637@qq.com>
2018-05-05 22:44:53 +0800

    #第一个demo 添加图片

--- app/src/main/java/com/onlybeyond/crack/TestActivity.java
+++ app/src/main/java/com/onlybeyond/crack/TestActivity.java
@@ -22,26 +22,13 @@ public class TestActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         testLog();
     }
-
     /**
      * 用于测试release版修改debug版
      * release 包不会打印debug级别日志
      */
     private void testLog() {
-        LogUtils.LOGD(TAG, "这是一个调试日志");
-        LogUtils.LOGE(TAG, "错误类型的日志");
+        LogUtils.LOGD(TAG, "!!!这是一个调试日志");
+        LogUtils.LOGE(TAG, "!!!错误类型的日志");
     }
 
-
-    void test() {
-        int i = 3;
-        i = 5;
-        if (i < 3) {
-            Toast.makeText(this, "i 小于3" + i, Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "i 大于等于3", Toast.LENGTH_SHORT).show();
-
-        }
-        Log.e("test", "---test" + i);
-    }
 }
---
+++ app/src/main/java/com/onlybeyond/crack/utils/InsertActivity.java
@@ -0,0 +1,18 @@
+package com.onlybeyond.crack.utils;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Description
+ * Created by only on 18/5/5.
+ * Email: onlybeyond99@gmail.com
+ */
+
+public class InsertActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
