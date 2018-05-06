0ff1010fa98314daf784f1cababc642947ac8877
only <932507637@qq.com>
2018-05-06 21:52:18 +0800

    #第二个demo 修改

--- app/assets/demo_tow/InsertActivity.java
+++ app/assets/demo_tow/InsertActivity.java
@@ -1,8 +1,15 @@
-package com.onlybeyond.crack.utils;
+package com.onlybeyond.crack;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Description
@@ -11,8 +18,21 @@ import android.support.v4.app.FragmentActivity;
  */
 
 public class InsertActivity extends FragmentActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        // 为了能看到页面的交互添加了一个延迟。
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName("com.onlybeyond.crack",
+                        "com.onlybeyond.crack.TestTwoActivity"));
+                startActivity(intent);
+            }
+        }, 3000);
     }
 }
